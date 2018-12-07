1cea7f8828e24a848110d20a6464d95500134ec2
Joshua Beatty <beattyjoshua55@Gmail.com>
2018-12-07 23:53:47 +0000

    Update Borrower.java

--- LibrarySystem/src/Borrower.java
+++ LibrarySystem/src/Borrower.java
@@ -3,14 +3,6 @@ import java.util.ArrayList;
 /**
  * This class describes the attributes and behaviour
  * of a lending library member.
- * 
- * Analyse and document this class by adding
- * JavaDoc and other comments to explain the purpose of
- * the instance variables and the behaviour of
- * the methods.
- * 
- * @author Paul Sage
- *
  */
 public class Borrower {
 	private String name;
